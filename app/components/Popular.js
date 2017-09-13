47e26ee8fbc287cec6229c702682111bcc9c6fd2
Akshay Kumar <akkisinghpanchaal@gmail.com>
2017-09-13 16:48:32 +0530

    selectLanguage is now a stateless functional component | an illustration of stateless functional component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,23 +1,25 @@
 var React = require('react');
-
-class SelectLanguage extends React.Component {
-  render () {
-    var languages = ['All','JavaScript','Ruby','Java','CSS','Python'];
-    return (
-      <ul className = "languages">
-        {languages.map(function(lang){
-          return(
-            <li
-            style = {lang===this.props.selectedLanguage ? {color:'#d0021b'}:null}
-            key={lang}
-            onClick={this.props.onSelect.bind(null,lang)}>
-              {lang}
-            </li>
-          )
-        },this)}
-      </ul>
-    )
-  }
+var PropTypes = require('prop-types');
+function SelectLanguage (props){
+  var languages = ['All','JavaScript','Ruby','Java','CSS','Python'];
+  return (
+    <ul className = "languages">
+      {languages.map(function(lang){
+        return(
+          <li
+          style = {lang===props.selectedLanguage ? {color:'#d0021b'}:null}
+          key={lang}
+          onClick={props.onSelect.bind(null,lang)}>
+            {lang}
+          </li>
+        )
+      },this)}
+    </ul>
+  )
+}
+SelectLanguage.propTypes={
+  selectedLanguage: PropTypes.string.isRequired,
+  onSelect: PropTypes.func.isRequired
 }
 
 class Popular extends React.Component {
